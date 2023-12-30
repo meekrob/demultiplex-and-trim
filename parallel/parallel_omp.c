55290c582caae7954faaeb2df9ba7601a6d14e45
David <davidcking.inbox@gmail.com>
2023-12-30 13:22:13 -0700

    Update parallel_omp.c

--- parallel/parallel_omp.c
+++ parallel/parallel_omp.c
@@ -1,1 +1,68 @@
-// new 
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+#define CHUNK_SIZE 1024 * 1024 * 1024  // 1 GB
+
+// Data structure to pass to each thread
+struct ThreadData {
+    int threadId;
+    char* sharedMemory;  // Pointer to shared memory
+    size_t chunkSize;    // Size of each chunk
+};
+
+// Function that each thread will execute
+void processChunk(struct ThreadData* data) {
+    // Process the chunk
+    size_t offset = data->threadId * data->chunkSize;
+    size_t end = offset + data->chunkSize;
+    if (end > data->chunkSize) {
+        end = data->chunkSize;
+    }
+
+    // Modify shared memory (data->sharedMemory) here based on the thread's chunk
+    // ...
+}
+
+int main() {
+    const char* filename = "your_gzipped_file.gz";
+
+    // Allocate shared memory for one chunk
+    char* sharedMemory = (char*)malloc(CHUNK_SIZE);
+    if (sharedMemory == NULL) {
+        fprintf(stderr, "Error allocating shared memory\n");
+        return EXIT_FAILURE;
+    }
+
+    // Read the entire file into shared memory
+    FILE* file = fopen(filename, "rb");
+    if (file == NULL) {
+        fprintf(stderr, "Error opening file %s\n", filename);
+        free(sharedMemory);
+        return EXIT_FAILURE;
+    }
+
+    size_t bytesRead = fread(sharedMemory, 1, CHUNK_SIZE, file);
+
+    fclose(file);
+
+    size_t totalBytesRead = bytesRead;
+
+    #pragma omp parallel
+    {
+        #pragma omp for schedule(dynamic) nowait
+        for (size_t i = 0; i < 3; ++i) {
+            struct ThreadData threadData;
+            threadData.threadId = i;
+            threadData.sharedMemory = sharedMemory;
+            threadData.chunkSize = bytesRead;
+
+            // Call processChunk in parallel
+            processChunk(&threadData);
+        }
+    }
+
+    free(sharedMemory);
+
+    return EXIT_SUCCESS;
+}
